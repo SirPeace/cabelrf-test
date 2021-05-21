a7e9826886d25c2a0d74a39f98e8095c4dbb96d2
SirPeace <roman.khabibulin12@gmail.com>
2021-05-21 13:59:36 +0300

    Add basic DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         (new RoleSeeder)->run();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Roman Khabibulin',
+            'email' => 'roman.khabibulin12@gmail.com',
+            'password' => Hash::make('admin'),
+            'role_id' => \App\Models\Role::where('name', 'admin')->first()->id,
+            'age' => 18,
+            'sex' => 'M'
+        ]);
+
+        (new ProductSeeder)->run();
     }
 }
