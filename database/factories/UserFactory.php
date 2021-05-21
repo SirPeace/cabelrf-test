97dd60688a3975577ed52f84f975cdffadfcee10
SirPeace <roman.khabibulin12@gmail.com>
2021-05-21 13:56:00 +0300

    Add factory fields on UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'age' => $this->faker->numberBetween(18, 60),
+            'sex' => $this->faker->randomElement(['M', 'F']),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
