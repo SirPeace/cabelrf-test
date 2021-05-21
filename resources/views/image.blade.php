0b5ec832d1a8022ec645dfd5be934357718e677e
SirPeace <roman.khabibulin12@gmail.com>
2021-05-21 13:59:55 +0300

    Create/update views

--- resources/views/image.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>Image</h1>
-<img src="{{ $thumbnail_url }}" alt="Image" />
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,7 +3,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <h1 class="text-xl font-bold mb-4 ">Products</h1>
+                    <x-product-table></x-product-table>
                 </div>
             </div>
         </div>
