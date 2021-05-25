112f897039e75cccb9257823a8b1a49c667b4c68
SirPeace <roman.khabibulin12@gmail.com>
2021-05-26 00:13:09 +0300

    Views authorization

--- resources/views/components/products-table.blade.php
+++ resources/views/components/products-table.blade.php
@@ -63,10 +63,12 @@
                       "
                       data-href="{{ route('products.show', compact('product')) }}">
                     <td class="pl-4 py-4 whitespace-nowrap">
-                      <input type="checkbox"
-                             name="product-id:{{ $product->id }}"
-                             class="rounded border-gray-500 cursor-pointer"
-                             @click="selected = event.target.checked ? selected + 1 : selected - 1">
+                      @can('manage-products')
+                        <input type="checkbox"
+                               name="product-id:{{ $product->id }}"
+                               class="rounded border-gray-500 cursor-pointer"
+                               @click="selected = event.target.checked ? selected + 1 : selected - 1">
+                      @endcan
                     </td>
                     <td class="pl-4 py-4 whitespace-nowrap">
                       <span class="text-sm text-gray-900 pl-2">{{ $product->id }}</span>
@@ -120,21 +122,23 @@
                       </span>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-3">
-                      <a href="{{ route('products.edit', ['product' => $product]) }}"
-                         class="text-indigo-600 hover:text-indigo-900" title="{{ __('Edit') }}">
-                        <svg class="inline h-5 w-5 relative right-2" viewBox="0 0 20 20" fill="currentColor">
-                          <path
-                                d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                        </svg>
-                      </a>
-                      <x-delete-product-button class="inline h-5 w-5 relative right-2 text-gray-500 hover:text-red-700"
-                                               :product="$product">
-                        <svg viewBox="0 0 20 20" fill="currentColor" class="inline">
-                          <path fill-rule="evenodd"
-                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                clip-rule="evenodd" />
-                        </svg>
-                      </x-delete-product-button>
+                      @can('manage-products')
+                        <a href="{{ route('products.edit', ['product' => $product]) }}"
+                           class="text-indigo-600 hover:text-indigo-900" title="{{ __('Edit') }}">
+                          <svg class="inline h-5 w-5 relative right-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                  d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                          </svg>
+                        </a>
+                        <x-delete-product-button class="inline h-5 w-5 relative right-2 text-gray-500 hover:text-red-700"
+                                                 :product="$product">
+                          <svg viewBox="0 0 20 20" fill="currentColor" class="inline">
+                            <path fill-rule="evenodd"
+                                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                  clip-rule="evenodd" />
+                          </svg>
+                        </x-delete-product-button>
+                      @endcan
                     </td>
                   </tr>
                 @endforeach
